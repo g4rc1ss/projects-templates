bc2dc4088526795b31c4d74453cf7e89fa70f323
Asier <asiergarciab@outlook.com>
2025-03-24 11:42:23 +0100

    Agregar if Postgres

--- dotnet/WebCleanArchitecture/src/Template.AspireHost/Program.cs
+++ dotnet/WebCleanArchitecture/src/Template.AspireHost/Program.cs
@@ -2,6 +2,22 @@ using Projects;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
+IResourceBuilder<ParameterResource> username = builder.AddParameter("username", "postgres");
+IResourceBuilder<ParameterResource> password = builder.AddParameter("password", "123456");
+
+#if (UsePostgres)
+IResourceBuilder<PostgresDatabaseResource> postgres = builder
+    .AddPostgres("PostgresRN", username, password, 5432)
+    .WithDataVolume("postgresVM", isReadOnly: false)
+    .WithLifetime(ContainerLifetime.Session)
+    .AddDatabase("PostgresDB", "RedNeuronal");
+#endif
+#if (UsePostgres)
+builder.AddProject<Template_HostWebApi>("Template")
+    .WithReference(postgres, "DatabaseContext")
+    .WaitFor(postgres);
+#else
 builder.AddProject<Template_HostWebApi>("Template");
+#endif
 
 builder.Build().Run();
