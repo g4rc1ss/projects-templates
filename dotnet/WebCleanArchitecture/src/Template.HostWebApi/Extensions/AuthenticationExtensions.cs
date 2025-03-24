f40480e01f805767bb8cb965e6558c0a7ff8d071
Asier <asiergarciab@outlook.com>
2025-03-24 11:35:42 +0100

    Eliminar UserSecrets

--- dotnet/WebCleanArchitecture/src/Template.HostWebApi/Extensions/AuthenticationExtensions.cs
+++ dotnet/WebCleanArchitecture/src/Template.HostWebApi/Extensions/AuthenticationExtensions.cs
@@ -1,3 +1,16 @@
+#if (UseIdentity)
+using Infraestructure.Database;
+using Infraestructure.Database.Entities;
+using Microsoft.AspNetCore.Identity;
+#endif
+#if (UseIdentity || UseJwt)
+using AuthManager.API;
+using AuthManager.API.ConfigurationOptions;
+using AuthManager.Application.Contracts;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+#endif
+
 namespace Template.HostWebApi.Extensions;
 
 public static class AuthenticationExtensions
@@ -5,9 +18,53 @@ public static class AuthenticationExtensions
     internal static void AddAuthenticationProtocol(this IServiceCollection services,
         IConfiguration configuration)
     {
-        // services
-        //     .AddHttpContextAccessor()
-        //     .AddAuthorization()
-        //     .AddAuthentication();
+#if (UseIdentity || UseJwt)
+        services
+            .AddHttpContextAccessor()
+            .AddAuthorization()
+            .AddAuthentication(options =>
+            {
+            })
+            .AddJwtBearer(options =>
+            {
+                IConfigurationSection jwtOptionsSection = configuration
+                    .GetSection("Jwt");
+
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    RequireExpirationTime = true,
+                    ValidIssuer = jwtOptionsSection["Issuer"],
+                    ValidAudience = jwtOptionsSection["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtOptionsSection["Key"]!))
+                };
+            });
+        services.AddScoped<IJwtTokenManagement, JwtTokenManagement>();
+        services.Configure<JwtOption>(configuration.GetSection("Jwt"));
+#endif
+
+#if (UseIdentity)
+        services.AddIdentityCore<UserEntity>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.User.RequireUniqueEmail = true;
+
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            })
+            .AddUserManager<UserManager<UserEntity>>()
+            .AddRoles<RoleEntity>()
+            .AddRoleManager<RoleManager<RoleEntity>>()
+            .AddDefaultTokenProviders()
+            .AddEntityFrameworkStores<DatabaseContext>();
+#endif
+
     }
 }
