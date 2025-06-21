c71d79254fdb10f0a7b15d952a1dd46fa9b04eee
Asier <asiergarciab@outlook.com>
2025-06-21 16:10:14 +0200

    Update CosmosDbEntity.cs

--- dotnet/WebInitializer/Infraestructure.Database/Entities/CosmosDbEntity.cs
+++ dotnet/WebInitializer/Infraestructure.Database/Entities/CosmosDbEntity.cs
@@ -1,10 +1,7 @@
-using MongoDB.Bson.Serialization.Attributes;
-
 namespace Infraestructure.Database.Entities;
 
 public class CosmosDbEntity
 {
-    [BsonId]
     public required string Id { get; set; }
 
     public string? Property { get; set; }
