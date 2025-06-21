219c53ba1711bd3f8beb8b7020e7592607d350e7
Asier <asiergarciab@outlook.com>
2025-06-21 12:01:52 +0200

    create powershell remove nuget unused

---
+++ dotnet/RemoveNuget.ps1
@@ -0,0 +1,30 @@
+$packages = Select-Xml -Path "Directory.Packages.props" -XPath "//PackageVersion"
+$unusedPackages = @()
+
+foreach ($pkgNode in $packages) {
+    $pkg = $pkgNode.Node.Include
+    $found = Get-ChildItem -Recurse -Include *.csproj, Directory.Build.props -File | Select-String -Pattern $pkg
+
+    if (-not $found) {
+        Write-Output "$pkg not used in any .csproj"
+        $unusedPackages += $pkgNode.Node.Include
+    }
+}
+
+if ($unusedPackages.Count -eq 0) {
+    Write-Output "No unused packages found."
+    exit 0
+}
+
+[xml]$doc = Get-Content "Directory.Packages.props" -Raw
+
+foreach ($unused in $unusedPackages) {
+    $node = $doc.SelectSingleNode("//PackageVersion[@Include='$unused']")
+    if ($node -ne $null) {
+        $null = $node.ParentNode.RemoveChild($node)
+        Write-Host "Removed $unused"
+    }
+}
+
+$doc.Save("Directory.Packages.props")
+Write-Output "Updated Directory.Packages.props with unused packages removed."
