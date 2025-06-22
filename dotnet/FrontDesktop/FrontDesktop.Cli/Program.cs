1e813fad74c9976060d6244f25ba3fc78cb144a4
Asier <asiergarciab@outlook.com>
2025-06-22 09:48:45 +0200

    feat: Add CLI on Desktop template

---
+++ dotnet/FrontDesktop/FrontDesktop.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.CommandLine;
+
+RootCommand? rootCommand = new(
+    "Ejemplo de la nueva api de Microsoft para Argumentos de comandos para CLI"
+);
+
+Option<FileInfo>? fileOpt = new(name: "--file") { Description = "The file to read" };
+rootCommand.Options.Add(fileOpt);
+
+ParseResult parseResult = rootCommand.Parse(args);
+
+FileInfo getFile = parseResult.GetRequiredValue(fileOpt);
+Console.WriteLine(await File.ReadAllTextAsync(getFile.FullName));
+
+// --file texto_prueba.txt
