20b8e02ad304d485a20382079426ba22146c245c
Asier <asiergarciab@outlook.com>
2025-06-28 17:25:57 +0200

    feat: Add ROP

---
+++ dotnet/Architectures/NLayerArchitecture/NLayerArchitecture.Application/Contracts/IApplicationContract.cs
@@ -0,0 +1,23 @@
+using Garciss.ROP;
+
+namespace NLayerArchitecture.Application.Contracts;
+
+public interface IApplicationContractBase;
+
+public interface IApplicationContract<in TRequest, TResponse> : IApplicationContractBase
+{
+    Task<Result<TResponse>> ExecuteAsync(
+        TRequest request,
+        CancellationToken cancellationToken = default
+    );
+}
+
+public interface IApplicationContract<in TRequest> : IApplicationContractBase
+{
+    Task<Result> ExecuteAsync(TRequest request, CancellationToken cancellationToken = default);
+}
+
+public interface IApplicationContract : IApplicationContractBase
+{
+    Task<Result> ExecuteAsync(CancellationToken cancellationToken = default);
+}
